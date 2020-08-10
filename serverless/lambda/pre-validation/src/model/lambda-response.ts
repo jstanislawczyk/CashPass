977168adad633777de2602e07cf96cd81a8dccfc
stanislawczykj <jakubstanislawczyk98@gmail.com>
2020-08-10 22:03:39 +0200

    Default error code is set to 500

--- serverless/lambda/pre-validation/src/model/lambda-response.ts
+++ serverless/lambda/pre-validation/src/model/lambda-response.ts
@@ -9,7 +9,7 @@ export class LambdaResponse {
   @IsPositive()
   public readonly statusCode: number;
 
-  constructor(body: Record<string, unknown> | string, statusCode: number) {
+  constructor(body: Record<string, unknown> | string, statusCode: number = 500) {
     this.body = body;
     this.statusCode = statusCode;
   }
